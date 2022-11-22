4e0fd6ef02fc1ba253ba4034f6f0aa951930c24a
claudia de jesus dantas <101577825+claudiadejesusdantas@users.noreply.github.com>
2022-11-22 18:23:31 -0300

    alteração no módulo App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { ApplicationRoutes } from "./Router"
+import { Footer } from "./pages/Footer"
 import './global.css'
+import './styles/components/footer.css'
 
 function App() {
   return (
     <div className="App">
       <ApplicationRoutes/>
+      <Footer text="Claudia de Jesus Dantas"/>
     </div>
   )
 }
